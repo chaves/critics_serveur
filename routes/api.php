1a528213748b922ed00f929968d9131cffc93dd7
Bruno Chaves <chabruno@gmail.com>
2019-05-12 10:29:38 +0200

    first commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('tweets{name}', 'TweetsController@index')->name('tweets');
+
+Route::post('tweets/update', 'TweetsController@update');
